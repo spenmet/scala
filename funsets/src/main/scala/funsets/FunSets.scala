ed1d4594f78a7f05967b1163774f1d34884c3a8f
Satish <satish.penmethsa@gmail.com>
2013-10-01 23:18:32 -0400

    changes added to tests

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -38,7 +38,7 @@ object FunSets {
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = (x:Int) => !contains(intersect(s,t), x)
+  def diff(s: Set, t: Set): Set = (x:Int) => !contains(intersect(s,t), x) && contains(s, x)
 
   /**
    * Returns the subset of `s` for which `p` holds.
--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -77,6 +77,11 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+
+    val positive100Nums:Set = (x:Int) => x > 0 && x < 101
+    val negative100Nums:Set = (x:Int) => x > -101 && x < 0
+    val negativeNums:Set = (x:Int) => x < 0
+    val positiveNums:Set = (x:Int) => x > 0
   }
 
   /**
@@ -108,5 +113,96 @@ class FunSetSuite extends FunSuite {
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
     }
+  }
+
+  test("intersect contains only common elements") {
+    new TestSets {
+      val s = intersect(positiveNums, negativeNums)
+      assert(!contains(s, 1))
+      assert(!contains(s, -1))
+
+      val s4 = intersect(_ % 2 == 0, _ % 3 == 0)
+      assert(contains(s4, 6))
+      assert(contains(s4, -12))
+      assert(!contains(s4, 9))
+
+    }
+  }
+
+  test("diff returns the difference of two sets") {
+    new TestSets {
+      val s = diff(positiveNums, _ % 2 == 0)
+      assert(contains(s, 1))
+      assert(contains(s, 33))
+      assert(!contains(s, 2))
+      assert(!contains(s, 44))
+
+      val s4 = diff(_ % 2 == 0, _ % 3 == 0)
+      assert(!contains(s4, 6))
+      assert(!contains(s4, -12))
+      assert(contains(s4, 8))
+      assert(contains(s4, -14))
+
+      val s5:Set = (x:Int) => x > 0 && x < 10
+      val s6 = diff(s5, _ % 2 == 0)
+      assert(FunSets.toString(s6) === "{1,3,5,7,9}")  
+
+    }
+  }
+
+  
+
+  test("filter returns the subset of `s` for which `p` holds.") {
+    new TestSets {
+      val s = filter(positiveNums, negativeNums)
+      assert(!contains(s, 1))
+      assert(!contains(s, -1))
+
+      val s4 = filter(negativeNums, _ < -10)
+      assert(contains(s4, -11))
+      assert(contains(s4, -1000))
+      assert(!contains(s4, 0))
+      assert(!contains(s4, -10))
+      assert(!contains(s4, -1))
+
+    }
+  }
+
+  test("exists returns true if there is a bounded integer that statisfies p") {
+    new TestSets {
+      assert(exists(_ < -900, _ % 100 == 0))
+      assert(exists(_ < -900, _ % 1000 == 0))
+      assert(exists(_ < -900, _ <  1000))
+      assert(!exists(_ < -900, _ <  -1950))
+      assert(exists(_ < -900, _ <  -950))
+      assert(exists(_ < -900, _ == -950))
+    }
+  }
+
+  test("forall returns true if all bounded integers statisfy p") {
+    new TestSets {
+      
+      assert(!forall((x:Int) => x % 2 == 0, (y:Int) => y % 4 == 0))
+      assert(forall((x:Int) => x % 4 == 0, (y:Int) => y % 2 == 0))
+      assert(!forall((x:Int) => x % 4 == 0, (y:Int) => y % 3 == 0))
+      assert(forall((x:Int) => x > 1000, (y:Int) => y < -1))
+      assert(forall((x:Int) => x < -1000, (y:Int) => y > 1))
+    }
+  }
+
+  test("map returns a set transformed by applying `f` to each element of `s`") {
+   new TestSets {
+      val s4 = map((x:Int) => x > 0 && x < 5, (y:Int) => y * y)
+      assert(contains(s4, 1))
+      assert(contains(s4, 4))
+      assert(contains(s4, 9))
+      assert(contains(s4, 16))
+
+      assert(!contains(s4, 2))
+      assert(!contains(s4, 3))
+      assert(!contains(s4, 25))    
+
+      assert(FunSets.toString(s4) === "{1,4,9,16}")  
+    } 
   }
 }
